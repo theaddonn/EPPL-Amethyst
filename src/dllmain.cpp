f6c9697196e1f84f741f7f7f0f9034d6d61ea31d
Lucy <theaddonn@gmail.com>
2024-07-22 15:22:47 +0200

    Initial commit

---
+++ src/dllmain.cpp
@@ -0,0 +1,20 @@
+﻿#include "dllmain.hpp"
+
+// Subscribed to amethysts on start join game event in Initialize
+void OnStartJoinGame(OnStartJoinGameEvent& event)
+{
+    Log::Info("The player has joined the game!");
+}
+
+// Ran when the mod is loaded into the game by AmethystRuntime
+ModFunction void Initialize(AmethystContext& ctx) 
+{
+    // Initialize Amethyst mod backend
+    InitializeAmethystMod(ctx);
+
+    // Logging from <Amethyst/Log.h>
+    Log::Info("Hello, Amethyst World!");
+
+    // Add a listener to a built in amethyst event
+    GetEventBus().AddListener<OnStartJoinGameEvent>(&OnStartJoinGame);
+}
---
+++ src/dllmain.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <Windows.h>
+#include <amethyst/runtime/ModContext.hpp>
+#include <amethyst/runtime/events/GameEvents.hpp> // OnStartJoinGameEvent
+
+#define ModFunction extern "C" __declspec(dllexport)
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    return TRUE;
+}
+
+void OnStartJoinGame(OnStartJoinGameEvent& event);
