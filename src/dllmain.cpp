08c5c21ac40037dee1c8ac0d36a20fea056efdb1
theaddon <theaddonn@gmail.com>
2024-08-04 07:53:04 +0200

    use sigs for addresses

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -15,6 +15,6 @@ ModFunction void Initialize(AmethystContext& ctx)
 	// setbe (set byte if below or equal) instruction, this all means that the
 	// number to compare against is limited to 8 bits (255 is the maximum) and now
 	// is always is atleast less or even equal to it.
-	Amethyst::GetPatchManager().ApplyPatch(SlideAddress(0x32D38EC), (uint8_t)0xFF);
-	Amethyst::GetPatchManager().ApplyPatch(SlideAddress(0x32D409B), (uint8_t)0xFF);
+	Amethyst::GetPatchManager().ApplyPatch(SigScan("48 89 5C 24 18 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24 90 48 81 EC 70 01 00 00 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 45 60 48 8B FA") + 0x15C, (uint8_t)0xFF);
+	Amethyst::GetPatchManager().ApplyPatch(SigScan("40 53 55 56 57 41 54 41 55 41 56 41 57 48 83 EC 68 48 8B 05 ?? ?? ?? ?? 48 33 C4 48 89 44 24 50 0F") + 0x54B, (uint8_t)0xFF);
 }
